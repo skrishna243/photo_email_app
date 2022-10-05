11b2d21449a8894df6823a859f310fc48da7efef
skrishna243 <32722755+skrishna243@users.noreply.github.com>
2022-10-05 12:14:59 -0400

    new request changes

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,7 +5,7 @@ class ReportsController < ApplicationController
 			@start_date          = Date.new( params[:dates_one]["date_from1(1i)"].to_i, params[:dates_one]["date_from1(2i)"].to_i, params[:dates_one]["date_from1(3i)"].to_i).to_date.beginning_of_day
             @end_date            = Date.new( params[:dates_one]["date_to1(1i)"].to_i, params[:dates_one]["date_to1(2i)"].to_i, params[:dates_one]["date_to1(3i)"].to_i).to_date.end_of_day
           
-			@stu_data = Student.where("event_date between ? and ? AND school_name = ? AND season_type = ?", @start_date, @end_date , params[:school_name], params[:season_name])
+			@stu_data = Student.where("event_date between ? and ? AND school_name = ? AND season_type = ? AND team_type = ? AND type_of_sport = ?", @start_date, @end_date , params[:school_name], params[:season_name], params[:team_type], params[:sport_type])
 		end
 		@source_company_names = School.where(user_id: current_user.id).pluck(:school_name).uniq
 	end
