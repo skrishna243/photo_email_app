c3b9189f768d3b6eecd48e97ef6ea77310759fcd
skrishna243 <32722755+skrishna243@users.noreply.github.com>
2023-11-15 17:47:37 -0500

    student id field can update in check IN/OUT time

--- app/controllers/senior_students_controller.rb
+++ app/controllers/senior_students_controller.rb
@@ -41,7 +41,7 @@ class SeniorStudentsController < ApplicationController
 			@senior_student_check = SeniorStudentCheck.where(school_id: params[:senior_school_id], internal_student_id: params[:student_id], check_type: 1).last
 			if @senior_student_check.present?
 				new_email_entry =  params[:student_email] == params[:check_in_student_email] ? "NO" : "YES"
-				 @senior_student_check.update(check_type: 2, email: params[:student_email], full_name: params[:student_full_name], new_data: new_email_entry)
+				 @senior_student_check.update(check_type: 2, email: params[:student_email], full_name: params[:student_full_name], new_data: new_email_entry, subject_id: params[:student_subject_id])
 				 flash[:success] = "Student successfully Checked Out"
 				 render json: @senior_student_check
 			else
@@ -57,10 +57,10 @@ class SeniorStudentsController < ApplicationController
 					@real_session_type = params[:session_type].map(&:to_i)
 					unless params[:check_in_new_data] == "YES"
 						new_email_entry =  params[:student_email] == params[:check_in_student_email] ? "NO" : "YES"
-						@senior_student_check.update(email: params[:student_email], full_name: params[:student_full_name], new_data: new_email_entry, session_type: @real_session_type )
+						@senior_student_check.update(email: params[:student_email], full_name: params[:student_full_name], new_data: new_email_entry, session_type: @real_session_type, subject_id: params[:student_subject_id]  )
 					 	flash[:success] = "Student data successfully updated"
 					else
-						@senior_student_check.update(email: params[:student_email], full_name: params[:student_full_name], session_type: @real_session_type )
+						@senior_student_check.update(email: params[:student_email], full_name: params[:student_full_name], session_type: @real_session_type, subject_id: params[:student_subject_id] )
 					 	flash[:success] = "Student data successfully updated"
 					end
 			else
